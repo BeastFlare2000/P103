a49797472737163a475758543778909ba087d8bb
BeastFlare2000 <73615807+BeastFlare2000@users.noreply.github.com>
2023-03-21 20:38:32 -0400

    Add files via upload

---
+++ P103-Viruses/P103.py
@@ -0,0 +1,31 @@
+import sys
+import time
+import random
+import os
+import shutil
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+
+from_dir = 'C:/Users/pl787/OneDrive/Desktop/Python/P103-Viruses'
+
+class FileEventHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        print(f'Hey, someone created {event.src_path}!')
+    def on_deleted(self, event):
+        print(f'Oops, someone deleted {event.src_path}!')
+    def on_modified(self, event):
+        print(f'Woah, some modified {event.src_path}!')
+    def on_moved(self, event):
+        print(f'Huh, someone moved {event.src_path}!')
+
+event_handler = FileEventHandler()
+observer = Observer()
+observer.schedule(event_handler, from_dir, recursive=True)
+observer.start()
+try:
+    while True:
+        time.sleep(2)
+        print('Running')
+except KeyboardInterrupt:
+    print('Stopped')
+    observer.stop()
